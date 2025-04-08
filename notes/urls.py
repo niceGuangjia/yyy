8683573b9197eba60594b03134d115312b8cb427
niceGJ <haoguangjia@gmail.com>
2025-04-08 09:46:33 +0800

    message 4: First unit test

--- notes/urls.py
+++ notes/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from lists import views
+# from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', views.home_page, name='home'),  # (1)
+    # path('admin/', admin.site.urls),  # (2)
 ]
