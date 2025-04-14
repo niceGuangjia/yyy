b9aa13067a2a7426ad00c484a5211a22d34880bb
niceGJ <haoguangjia@gmail.com>
2025-04-14 11:06:25 +0800

    message 14: add a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -78,7 +78,50 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 张三想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
-        self.fail('Finish the test!')
+        """ self.fail('Finish the test!') """
+
+    def test_multiple_uesrs_start_a_lists_at_different_urls(self):
+        # 张三开始了一个新清单，并看到有一个唯一的URL
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy flowers')
+
+        # 他注意到他的清单URL中包含了一个唯一的编号
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url, '/lists/.+')    # (1)
+
+        # 现在一个新用户王五访问网站
+        # 我们使用一个新的浏览器会话来确保cookie不共享，确保张三的信息不会泄露
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 王五访问首页，看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertNotIn('Give a gift to Lisi', page_text)
+
+        # 王五开始一个新清单，他输入一个不同的待办事项
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy milk')
+
+        # 王五获得了一个唯一的URL
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url, '/lists/.+')
+        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+
+        # 这个页面没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两个人都很满意
 
 
 
